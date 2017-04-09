52965315713dd9d7ada05f064b6cb6a7e7fa8014
duub qnnp <qnnp@duub.org>
2017-04-09 21:01:56 +0200

    base+static pages
    En aquest commit s'hi troba el codi del CI amb algunes pàgines estàtiques afegides seguint el tutorial del CI.

---
+++ application/views/pages/home.php
@@ -0,0 +1,5 @@
+<p>Hola Món!</p>
+
+<p>Benvingut/da a la pàgina estàtica home.</p>
+
+<p> Aquest exemple serveix per veure com es poden gestionar diverses pàgines estàtiques amb CI. Els fitxers que s'han afegit o modificat es poden veure en <a href="https://github.com/duub/CI-static-pages/commit/897138378091c370033468fee2e59005b37d21a7">aquest commit</a>.</p>
